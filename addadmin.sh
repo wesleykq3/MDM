b01aa42e4aab79a246bca23c930b4f94812a8361
wesleykq3 <1136895023@qq.com>
2024-11-05 11:16:01 +0800

    Add files via upload

--- addadmin.sh
+++ addadmin.sh
@@ -4,7 +4,7 @@ read username
 username="${username:=admin}"
 read passw
 passw="${passw:=1234}"
-dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
+dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
@@ -15,4 +15,9 @@ mkdir "/Volumes/Data/Users/$username"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
 dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
 dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+touch /Volumes/Data/private/var/db/.AppleSetupDone
+rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 exit 0
