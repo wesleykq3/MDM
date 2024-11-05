b26f7e98c98f283a26b2742c0652e01c62596cba
wesleykq3 <1136895023@qq.com>
2024-11-05 10:55:47 +0800

    Add files via upload
    在恢复模式添加账户

---
+++ addadmin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read username
+username="${username:=admin}"
+read passw
+passw="${passw:=1234}"
+dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+mkdir "/Volumes/Data/Users/$username"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+exit 0
